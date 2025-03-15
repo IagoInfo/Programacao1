a05ad0b0119d2119648120c7bc1d3e7d4addb3c3
IagoInfo <128693aluno@etmsl.com.br>
2025-03-14 21:24:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+  <!-- Sistema para automatizar preenchimento -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Automação</title>
+    </head>
+    <body>
+        <!-- Criar a div principal -->
+        <div class="usuario">
+            <!-- Criar a área para o formulário -->
+            <div class="form">
+                <!-- Criar formulário -->
+                <form> 
+                    <!-- Criar input para Nome do usuário -->
+                    <input type="text" name = "usuario" placeholder="Digite seu nome completo" required>
+                    
+                    <!-- Criar input para o Telefone do usuário --> 
+                    <input type="text" name = "telefone" placeholder="(00) 00000-0000" required>
+                    
+                    <!-- Criar input para a senha do usuário -->
+                    <input type="password" name = "senha" placeholder="Digite sua senha" required>
+                    
+                    <button>Salvar</button>
+                </form>
+            </div>
+        </div>
+    </body>
+    </html>
