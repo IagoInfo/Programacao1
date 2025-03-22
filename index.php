3cdc203dd10d7c1ce540626dea8d1aab2f4559ad
IagoInfo <128693aluno@etmsl.com.br>
2025-03-21 21:48:31 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Automação</title>
+        <link rel="stylesheet" href="./css/estilo.css">
+        <!-- Adicionar logo ao título-->
+        <link rel="shortcut icon" type="image/x-icon" href="./img/logo.png">
     </head>
     <body>
         <!-- Criar a div principal -->
